6e2f6304b84199bce0af5025f304f8d91e3b94ad
iosifvieru <iosifvieru440@gmail.com>
2023-12-29 13:37:42 +0200

    test

--- main.c
+++ main.c
@@ -9,10 +9,6 @@ int main(void){
 
 	
 	calculateArea("Rectangle area: %r \nCircle Area: %c \nSHAPE: %s\n", rectangle, circleTest, shape);
-		
-
-//	printf("SHAPE: Area: %lf, Perimeter: %lf\n", shape.area(&shape), shape.perimeter(&shape));
-//	printf("CIRCLE: Area: %lf, Perimeter: %lf\n", circleTest.base.area(&circleTest), circleTest.base.perimeter(&circleTest));
 //	printf("RECTANGLE: Area: %lf, Perimeter: %lf\n", rectangle.base.area(&rectangle), rectangle.base.perimeter(&rectangle));
 
 	return 0;
